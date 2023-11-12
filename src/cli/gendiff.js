fc228bb4f0cb2e3ca8cabbcd7da3e3bfa6166b4a
Darkon <9017059@gmail.com>
2023-11-12 11:18:12 +0300

    update: second step complete

---
+++ src/cli/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format (default: "stylish")')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
+program.parse();
