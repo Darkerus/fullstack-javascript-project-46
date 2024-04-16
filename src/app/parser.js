2527c3f15815ac653e773833eded75c6c4572800
Darkon <9017059@gmail.com>
2024-04-16 09:49:09 +0300

    step 6 - add: yaml parser

--- src/app/parser.js
+++ src/app/parser.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import jsYaml from 'js-yaml';
 import path from 'path';
 import { cwd } from 'process';
 
@@ -6,18 +7,19 @@ export default function takeFiles(path1, path2) {
   path1 = takeFilePath(path1);
   path2 = takeFilePath(path2);
 
-  const format1 = path1.split('.').at(-1) || null;
-  const format2 = path2.split('.').at(-1) || null;
+  const format1 = path.extname(path1);
+  const format2 = path.extname(path2);
 
   const file1 = fs.readFileSync(path1);
   const file2 = fs.readFileSync(path2);
 
-  const formatDispetcher = {
-    json: (data) => JSON.parse(data),
-  };
+  const formatDispetcher = new Map();
+  formatDispetcher.set('.json', (data) => JSON.parse(data));
+  formatDispetcher.set('.yaml', (data) => jsYaml.load(data));
+  formatDispetcher.set('.yml', (data) => jsYaml.load(data));
 
-  const data1 = formatDispetcher[format1](file1);
-  const data2 = formatDispetcher[format2](file2);
+  const data1 = formatDispetcher.get(format1)(file1);
+  const data2 = formatDispetcher.get(format2)(file2);
 
   return { data1, data2 };
 }
--- tests/app/parser.test.js
+++ tests/app/parser.test.js
@@ -1,6 +1,6 @@
 import fileParser from '../../src/app/parser.js';
 
-test('fileParser', () => {
+test('JSON - fileParser', () => {
   const { expect1, expect2 } = {
     expect1: {
       host: 'hexlet.io',
@@ -16,6 +16,26 @@ test('fileParser', () => {
   };
 
   const { data1, data2 } = fileParser('./__fixtures__/file1.json', './__fixtures__/file2.json');
+  expect(data1).toEqual(expect1);
+  expect(data2).toEqual(expect2);
+});
+
+test('JSON - fileParser', () => {
+  const { expect1, expect2 } = {
+    expect1: {
+      host: 'hexlet.io',
+      timeout: 50,
+      proxy: '123.234.53.22',
+      follow: false,
+    },
+    expect2: {
+      timeout: 20,
+      verbose: true,
+      host: 'hexlet.io',
+    },
+  };
+
+  const { data1, data2 } = fileParser('./__fixtures__/file1.yml', './__fixtures__/file2.yml');
   expect(data1).toEqual(expect1);
   expect(data2).toEqual(expect2);
 });
