5b40713ad38d11a5ec5ccd808679f1d028d04de9
Darkon <9017059@gmail.com>
2024-04-18 20:33:46 +0300

    new tests for nested objects

--- tests/app/diff.test.js
+++ tests/app/diff.test.js
@@ -1,5 +1,8 @@
 import genDiff from '../../src/app/diff.js';
 import { test, expect } from '@jest/globals';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 let data1;
 let data2;
@@ -29,4 +32,58 @@ test('genDiff - normal working', () => {
 }`;
 
   expect(genDiff(data1, data2)).toBe(result);
+});
+
+test('gendiff - nested files', () => {
+  const __dirname = fileURLToPath(import.meta.url);
+  const data1 = JSON.parse(fs.readFileSync(path.join(__dirname, './../../../__fixtures__/file1nested.json')));
+  const data2 = JSON.parse(fs.readFileSync(path.join(__dirname, './../../../__fixtures__/file2nested.json')));
+
+  const expected = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+
+  const result = genDiff(data1, data2);
+  expect(result).toEqual(expected);
 });
