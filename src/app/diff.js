021258334b5ad8e42470be0be68c20f4fbfc1f5c
Darkon <9017059@gmail.com>
2024-04-19 11:17:38 +0300

    try to do next step

--- src/app/diff.js
+++ src/app/diff.js
@@ -4,34 +4,49 @@ import _ from 'lodash';
  * @param {Record<string, any>} data1
  * @param {Record<string, any>} data2
  */
-export default function (data1, data2) {
+export default function genDiff(data1, data2) {
   const arrayOfData1 = Object.entries(data1);
   const arrayOfData2 = Object.entries(data2);
 
-  const diffMinus = arrayOfData1
-    .map(([key, value]) => {
-      if (!(key in data2)) return [key, value, '-'];
-      else if (value !== data2[key]) return [key, value, '-'];
-      else return [key, value, ''];
-    })
-    .filter((el) => el !== undefined);
+  const diff =
+    (data, symbol, depth = 0) =>
+    ([key, value]) => {
+      if (typeof value === 'object' && typeof data[key] === 'object' && !_.isNull(value) && !_.isNull(data[key]))
+        return genDiff(value, data2[key]);
+      if (!(key in data)) return [key, value, symbol, depth];
+      else if (value !== data[key]) return [key, value, symbol, depth];
+    };
 
-  const diffPlus = arrayOfData2
-    .map(([key, value]) => {
-      if (!(key in data1)) return [key, value, '+'];
-      else if (value !== data1[key]) return [key, value, '+'];
-    })
-    .filter((el) => el !== undefined);
+  const stable =
+    (data, depth = 0) =>
+    ([key, value]) => {
+      if (typeof value === 'object' && typeof data[key] === 'object' && !_.isNull(value) && !_.isNull(data[key]))
+        return genDiff(value, data2[key]);
+      if (!(key in data)) return undefined;
+      else if (value !== data[key]) return undefined;
+      else return [key, value, ' ', depth];
+    };
 
-  const diff = [...diffMinus, ...diffPlus];
-  const diffSort = _.sortBy(diff, [
+  const diffMinus = arrayOfData1.map(diff(data2, '-')).filter((el) => el !== undefined);
+
+  const diffPlus = arrayOfData2.map(diff(data1, '+')).filter((el) => el !== undefined);
+
+  const diffStable = arrayOfData1.map(stable(data2)).filter((el) => el !== undefined);
+
+  const diffSumm = [...diffMinus, ...diffPlus, ...diffStable];
+  const diffSort = _.sortBy(diffSumm, [
     0,
     (o) => {
       return -o[2];
     },
   ]);
 
-  const resultString = '{\n' + diffSort.reduce((acc, cur) => acc + `  ${cur[2]} ${cur[0]}: ${cur[1]}\n`, '') + '}';
+  const resultString =
+    '{\n' +
+    diffSort.reduce((acc, cur) => {
+      acc + `  ${cur[2]} ${cur[0]}: ${cur[1]}\n`, '';
+    }) +
+    '}';
 
-  return resultString;
+  return JSON;
 }
