fea7c2130c081290a8a1f08c00a9123ed53f7b2e
Darkon <9017059@gmail.com>
2024-04-20 09:56:12 +0300

    update: first work diffGen implementation

--- src/app/diff.js
+++ src/app/diff.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+
+let result = [];
+
 /**
  *
  * @param {Record<string, any>} data1
  * @param {Record<string, any>} data2
  */
-export default function genDiff(data1, data2) {
+export default function genDiff(data1, data2, depth = 0) {
   const arrayOfData1 = Object.entries(data1);
   const arrayOfData2 = Object.entries(data2);
 
@@ -12,7 +15,9 @@ export default function genDiff(data1, data2) {
     (data, symbol, depth = 0) =>
     ([key, value]) => {
       if (typeof value === 'object' && typeof data[key] === 'object' && !_.isNull(value) && !_.isNull(data[key]))
-        return genDiff(value, data2[key]);
+        return [];
+      if (typeof value === 'object' && typeof data[key] !== 'object' && !_.isNull(value))
+        return [key, genDiff(value, value, depth + 1), symbol, depth];
       if (!(key in data)) return [key, value, symbol, depth];
       else if (value !== data[key]) return [key, value, symbol, depth];
     };
@@ -21,17 +26,26 @@ export default function genDiff(data1, data2) {
     (data, depth = 0) =>
     ([key, value]) => {
       if (typeof value === 'object' && typeof data[key] === 'object' && !_.isNull(value) && !_.isNull(data[key]))
-        return genDiff(value, data2[key]);
+        return [key, genDiff(value, data2[key], depth + 1), ' ', depth];
       if (!(key in data)) return undefined;
       else if (value !== data[key]) return undefined;
       else return [key, value, ' ', depth];
     };
 
-  const diffMinus = arrayOfData1.map(diff(data2, '-')).filter((el) => el !== undefined);
+  const diffMinus = arrayOfData1
+    .map(diff(data2, '-', depth))
+    .filter((el) => el !== undefined)
+    .filter((el) => (Array.isArray(el) ? el.length > 0 : true));
 
-  const diffPlus = arrayOfData2.map(diff(data1, '+')).filter((el) => el !== undefined);
+  const diffPlus = arrayOfData2
+    .map(diff(data1, '+', depth))
+    .filter((el) => el !== undefined)
+    .filter((el) => (Array.isArray(el) ? el.length > 0 : true));
 
-  const diffStable = arrayOfData1.map(stable(data2)).filter((el) => el !== undefined);
+  const diffStable = arrayOfData1
+    .map(stable(data2, depth))
+    .filter((el) => el !== undefined)
+    .filter((el) => (Array.isArray(el) ? el.length > 0 : true));
 
   const diffSumm = [...diffMinus, ...diffPlus, ...diffStable];
   const diffSort = _.sortBy(diffSumm, [
@@ -41,12 +55,9 @@ export default function genDiff(data1, data2) {
     },
   ]);
 
-  const resultString =
-    '{\n' +
-    diffSort.reduce((acc, cur) => {
-      acc + `  ${cur[2]} ${cur[0]}: ${cur[1]}\n`, '';
-    }) +
-    '}';
+  // MARK: FORMATER
 
-  return JSON;
+  const stringResult = diffSort;
+
+  return depth === 0 ? JSON.stringify(diffSort) : diffSort;
 }
