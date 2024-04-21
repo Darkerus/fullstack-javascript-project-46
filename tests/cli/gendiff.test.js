19e42195f1a7fc38c4450983476fe1fe1517c4e3
Darkon <9017059@gmail.com>
2024-04-21 19:23:53 +0300

    fix: mutation in tests

--- tests/cli/gendiff.test.js
+++ tests/cli/gendiff.test.js
@@ -4,14 +4,7 @@ import program from '../../src/cli/gendiff.js';
 jest.spyOn(console, 'log');
 
 test('cli instrument', () => {
-  const result = {};
-
-  jest.mocked(console.log).mockImplementation((string) => {
-    result.res = string;
-  });
-
-  program.parse(['', '', './__fixtures__/file1nested.json', './__fixtures__/file2nested.json']);
-  const expectData = `{
+  const expectData1 = `{
     common: {
       + follow: false
         setting1: Value 1
@@ -55,19 +48,14 @@ test('cli instrument', () => {
         fee: 100500
     }
 }`;
-
-  expect(result.res).toEqual(expectData);
+  jest.mocked(console.log).mockImplementationOnce((string) => {
+    expect(string).toEqual(expectData1);
+  });
+  program.parse(['', '', './__fixtures__/file1nested.json', './__fixtures__/file2nested.json']);
 });
 
 test('e2e - plain formater', () => {
-  const result = {};
-
-  jest.mocked(console.log).mockImplementation((string) => {
-    result.res = string;
-  });
-
-  program.parse(['', '', './__fixtures__/file1nested.json', './__fixtures__/file2nested.json', '--format', 'plain']);
-  const expectData = `Property 'common.follow' was added with value: false
+  const expectData2 = `Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
 Property 'common.setting4' was added with value: 'blah blah'
@@ -79,18 +67,17 @@ Property 'group1.nest' was updated. From [complex value] to 'str'
 Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
 
-  expect(result.res).toEqual(expectData);
+  jest.mocked(console.log).mockImplementationOnce((string) => {
+    expect(expectData2).toEqual(string);
+  });
+  program.parse(['', '', './__fixtures__/file1nested.json', './__fixtures__/file2nested.json', '--format', 'plain']);
 });
 
 test('e2e - json formater', () => {
-  const result = {};
+  const expectData3 = '[{"path":"common.follow","key":"follow","process":"add","value":false},{"path":"common.setting2","key":"setting2","process":"remove","removedValue":200},{"path":"common.setting3","key":"setting3","process":"update","previousValue":true,"value":null},{"path":"common.setting4","key":"setting4","process":"add","value":"blah blah"},{"path":"common.setting5","key":"setting5","process":"add","value":"[complex value]"},{"path":"common.setting6.doge.wow","key":"wow","process":"update","previousValue":"","value":"so much"},{"path":"common.setting6.ops","key":"ops","process":"add","value":"vops"},{"path":"group1.baz","key":"baz","process":"update","previousValue":"bas","value":"bars"},{"path":"group1.nest","key":"nest","process":"update","previousValue":"[complex value]","value":"str"},{"path":"group2","key":"group2","process":"remove","removedValue":"[complex value]"},{"path":"group3","key":"group3","process":"add","value":"[complex value]"}]';
 
-  jest.mocked(console.log).mockImplementation((string) => {
-    result.res = string;
+  jest.mocked(console.log).mockImplementationOnce((string) => {
+    expect(expectData3).toEqual(string);
   });
-
   program.parse(['', '', './__fixtures__/file1nested.json', './__fixtures__/file2nested.json', '--format', 'json']);
-  const expectData = '[{"path":"common.follow","key":"follow","process":"add","value":false},{"path":"common.setting2","key":"setting2","process":"remove","removedValue":200},{"path":"common.setting3","key":"setting3","process":"update","previousValue":true,"value":null},{"path":"common.setting4","key":"setting4","process":"add","value":"blah blah"},{"path":"common.setting5","key":"setting5","process":"add","value":"[complex value]"},{"path":"common.setting6.doge.wow","key":"wow","process":"update","previousValue":"","value":"so much"},{"path":"common.setting6.ops","key":"ops","process":"add","value":"vops"},{"path":"group1.baz","key":"baz","process":"update","previousValue":"bas","value":"bars"},{"path":"group1.nest","key":"nest","process":"update","previousValue":"[complex value]","value":"str"},{"path":"group2","key":"group2","process":"remove","removedValue":"[complex value]"},{"path":"group3","key":"group3","process":"add","value":"[complex value]"}]';
-
-  expect(result.res).toEqual(expectData);
 });
